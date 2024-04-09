5092c6ba21c634fc66b0ef8cc576243735bba026
mohammad_tanveruzzaman <mohammad.tanveruzzaman@gmail.com>
2024-04-09 16:58:03 +0600

    Basic chart component unit test

--- src/app/Components/charts/charts.component.spec.ts
+++ src/app/Components/charts/charts.component.spec.ts
@@ -1,6 +1,9 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ChartsComponent } from './charts.component';
+import { GitgubSearchService } from 'src/app/Services/gitgub-search.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { BaseChartDirective, provideCharts, withDefaultRegisterables } from 'ng2-charts';
 
 describe('ChartsComponent', () => {
   let component: ChartsComponent;
@@ -8,7 +11,9 @@ describe('ChartsComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [ChartsComponent]
+      declarations: [ChartsComponent],
+      imports: [HttpClientTestingModule, BaseChartDirective],
+      providers: [GitgubSearchService, provideCharts(withDefaultRegisterables())]
     });
     fixture = TestBed.createComponent(ChartsComponent);
     component = fixture.componentInstance;
